4ed41131057473f2058352ed0b8c03f81501473c
Robin Landsgesell <robin.landsgesell@web.de>
2025-11-01 21:16:52 +0100

    ex3 init

---
+++ exercises/exercise03MeshNavigation/source.cpp
@@ -0,0 +1,280 @@
+#include <iostream>
+
+#include <Core/EntryPoint.h>
+#include <ATCG.h>
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <algorithm>
+#include <queue>
+
+#include <numeric>
+
+using VertexHandle = atcg::Mesh::VertexHandle;
+
+// Global mesh variable because we need it for the comparator
+std::shared_ptr<atcg::Mesh> mesh;
+OpenMesh::VPropHandleT<double> property_distance;
+OpenMesh::VPropHandleT<VertexHandle> property_previous;
+
+class DistanceComparator
+{
+public:
+    bool operator()(const VertexHandle vh1, const VertexHandle vh2)
+    {
+        return mesh->property(property_distance, vh1) > mesh->property(property_distance, vh2);
+    }
+};
+
+
+class Exercise03Layer : public atcg::Layer
+{
+public:
+    Exercise03Layer(const std::string& name) : atcg::Layer(name) {}
+
+    double distance_on_mesh(const std::shared_ptr<atcg::Mesh>& mesh,
+                            const VertexHandle& vh_start,
+                            const VertexHandle& vh_target)
+    {
+        double distance = std::numeric_limits<double>::infinity();
+
+        // Initialize the property with infinity to mark unvisited vertices
+        for(auto vh: mesh->vertices())
+        {
+            mesh->property(property_distance, vh) = std::numeric_limits<double>::infinity();
+        }
+
+        // The distance from the start vertex to itself is 0
+        mesh->property(property_distance, vh_start) = 0.0;
+
+        // A priority queue, which sorts the vertices by the distance to the source vertex
+        std::priority_queue<VertexHandle, std::vector<VertexHandle>, DistanceComparator> queue;
+        queue.push(vh_start);
+
+        // Process vertices as long as the queue is not empty and the target is not found
+        while(!queue.empty())
+        {
+            // TODO: Implement the loop body
+            // Hints:
+            // - Loop invariant: all vertices in the queue have a valid distance, which is sum of the lengths
+            //   of the edges on the shortest path between source_vh and the vertex.
+            // - The top vertex in the priority queue always has the shortest path from the source vertex of all
+            //   visited vertices.
+            // - Break the loop when the target vertex is found and assign its distance value to the "distance"
+            // variable.
+            // - The only case when the loop is terminated because of an empty queue is when the target vertex cannot be
+            // reached.
+            // - You can either use the Mesh::Point::norm method for distance calculations or the Mesh::calc_edge_length
+            // method,
+            //   depending on which type of iterators you use for finding neighboring vertices.
+
+            // 
+        }
+
+        return distance;
+    }
+
+    //// Excercise 3: Use the distance values to trace a path from the target vertex back to the source vertex ////
+    std::vector<VertexHandle> trace_back(const std::shared_ptr<atcg::Mesh>& mesh, const VertexHandle target_vh)
+    {
+        // Attention: This function only works after distance_on_mesh was executed with the same target_vh.
+
+        std::vector<VertexHandle> result_path;
+        VertexHandle current_vh = target_vh;
+        result_path.push_back(target_vh);
+
+        // TODO: Implement this
+        // Hints:
+        // - Each vertex on the path has a property with its previous vertex in the shortest path
+        // - The source vertex has distance 0.
+        // 
+        assert(mesh->property(property_distance, result_path.back()) == 0);
+
+        std::reverse(result_path.begin(), result_path.end());
+        return result_path;
+    }
+
+    // This is run at the start of the program
+    virtual void onAttach() override
+    {
+        const auto& window = atcg::Application::get()->getWindow();
+        float aspect_ratio = (float)window->getWidth() / (float)window->getHeight();
+        camera_controller  = std::make_shared<atcg::CameraController>(aspect_ratio);
+
+        mesh = atcg::IO::read_mesh("res/bunny.obj");
+
+        // Each vertex now holds a distance property (double)
+        mesh->add_property(property_distance);
+        mesh->add_property(property_previous);
+
+        mesh->request_vertex_colors();
+        for(auto v_it: mesh->vertices()) { mesh->set_color(v_it, atcg::Mesh::Color {255, 255, 255}); }
+
+        source_vh = mesh->vertex_handle(0);
+        target_vh = mesh->vertex_handle(4200);
+
+        mesh->uploadData();
+    }
+
+    // This gets called each frame
+    virtual void onUpdate(float delta_time) override
+    {
+        camera_controller->onUpdate(delta_time);
+
+        atcg::Renderer::clear();
+
+        if(mesh && render_faces)
+            atcg::Renderer::draw(mesh, atcg::ShaderManager::getShader("base"), camera_controller->getCamera());
+
+        if(mesh && render_points)
+            atcg::Renderer::drawPoints(mesh,
+                                       glm::vec3(0),
+                                       atcg::ShaderManager::getShader("base"),
+                                       camera_controller->getCamera());
+
+        if(mesh && render_edges) atcg::Renderer::drawLines(mesh, glm::vec3(0), camera_controller->getCamera());
+    }
+
+    virtual void onImGuiRender() override
+    {
+        ImGui::BeginMainMenuBar();
+
+        if(ImGui::BeginMenu("Rendering"))
+        {
+            ImGui::MenuItem("Show Render Settings", nullptr, &show_render_settings);
+            ImGui::EndMenu();
+        }
+
+        if(ImGui::BeginMenu("Exercise"))
+        {
+            ImGui::MenuItem("Show Dijkstra Settings", nullptr, &show_dijkstra_settings);
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMainMenuBar();
+
+        if(show_dijkstra_settings)
+        {
+            ImGui::Begin("Settings Dijkstra", &show_dijkstra_settings);
+
+            if(ImGui::Button("Calculate distance"))
+            {
+                final_distance   = distance_on_mesh(mesh, source_vh, target_vh);
+                clicked_distance = true;
+            }
+
+            if(ImGui::Button("Dijkstra"))
+            {
+                distance_on_mesh(mesh, source_vh, target_vh);
+                path             = trace_back(mesh, target_vh);
+                clicked_dijkstra = true;
+
+                if(path.size() != 112)
+                    error_msg << "Wrong path length";
+                else if(path.front() != source_vh || path.back() != target_vh)
+                    error_msg << "Wrong source or target";
+                // else
+                {
+                    // Color in vertices
+                    for(auto vh: path) { mesh->set_color(vh, atcg::Mesh::Color {255, 0, 0}); }
+                    mesh->uploadData();
+                }
+            }
+
+            if(clicked_distance)
+            {
+                ImGui::Text(("Distance between vertices: " + std::to_string(final_distance)).c_str());
+
+                if(3.0 > final_distance || final_distance > 3.1)
+                {
+                    ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, 0, 0, 1));
+                    ImGui::Text("Wrong distance!");
+                    ImGui::PopStyleColor();
+                }
+            }
+
+            if(clicked_dijkstra)
+            {
+                ImGui::PushStyleColor(ImGuiCol_Text, ImVec4(1, 0, 0, 1));
+                ImGui::Text(error_msg.str().c_str());
+                ImGui::PopStyleColor();
+            }
+
+            ImGui::End();
+        }
+
+        if(show_render_settings)
+        {
+            ImGui::Begin("Settings", &show_render_settings);
+
+            ImGui::Checkbox("Render Vertices", &render_points);
+            ImGui::Checkbox("Render Edges", &render_edges);
+            ImGui::Checkbox("Render Mesh", &render_faces);
+            ImGui::End();
+        }
+    }
+
+    // This function is evaluated if an event (key, mouse, resize events, etc.) are triggered
+    virtual void onEvent(atcg::Event& event) override
+    {
+        camera_controller->onEvent(event);
+
+        atcg::EventDispatcher dispatcher(event);
+        dispatcher.dispatch<atcg::FileDroppedEvent>(ATCG_BIND_EVENT_FN(Exercise03Layer::onFileDropped));
+    }
+
+    bool onFileDropped(atcg::FileDroppedEvent& event)
+    {
+        mesh = atcg::IO::read_mesh(event.getPath().c_str());
+
+        // Each vertex now holds a distance property (double)
+        mesh->add_property(property_distance);
+
+        mesh->request_vertex_colors();
+        for(auto v_it: mesh->vertices()) { mesh->set_color(v_it, atcg::Mesh::Color {255, 255, 255}); }
+
+        source_vh = mesh->vertex_handle(0);
+        target_vh = mesh->vertex_handle(4200);
+
+        mesh->uploadData();
+
+        // Also reset camera
+        const auto& window = atcg::Application::get()->getWindow();
+        float aspect_ratio = (float)window->getWidth() / (float)window->getHeight();
+        camera_controller  = std::make_shared<atcg::CameraController>(aspect_ratio);
+
+        return true;
+    }
+
+private:
+    std::shared_ptr<atcg::CameraController> camera_controller;
+
+    bool show_dijkstra_settings = true;
+    bool show_render_settings   = false;
+    bool render_faces           = true;
+    bool render_points          = false;
+    bool render_edges           = false;
+    double final_distance;
+    bool clicked_distance = false;
+    bool clicked_dijkstra = false;
+    std::vector<VertexHandle> path;
+    std::stringstream error_msg;
+
+    VertexHandle source_vh;
+    VertexHandle target_vh;
+};
+
+class Exercise03 : public atcg::Application
+{
+public:
+    Exercise03() : atcg::Application() { pushLayer(new Exercise03Layer("Layer")); }
+
+    ~Exercise03() {}
+};
+
+atcg::Application* atcg::createApplication()
+{
+    return new Exercise03;
+}
---
+++ exercises/exercise03Subdivision/source.cpp
@@ -0,0 +1,212 @@
+#include <iostream>
+
+#include <Core/EntryPoint.h>
+#include <ATCG.h>
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <algorithm>
+
+class Exercise03Layer : public atcg::Layer
+{
+public:
+    Exercise03Layer(const std::string& name) : atcg::Layer(name) {}
+
+    void subdivide_mesh(const std::shared_ptr<atcg::Mesh>& mesh)
+    {
+        // TODO: sqrt(3) subdivision
+        mesh->request_edge_status();
+        mesh->request_vertex_status();
+        uint32_t num_features = mesh->find_feature_edges();
+        std::cout << "Found " << num_features << " feature edges\n";
+
+        size_t nv = mesh->n_vertices();
+        size_t ne = mesh->n_edges();
+        size_t nf = mesh->n_faces();
+
+        auto eend = mesh->edges_end();
+        auto fend = mesh->faces_end();
+
+        auto new_pos_property = OpenMesh::makeTemporaryProperty<OpenMesh::VertexHandle, atcg::Mesh::Point>(*mesh.get());
+
+        // Calculate new position of old vertices
+        for(auto v_it = mesh->vertices_begin(); v_it != mesh->vertices_end(); ++v_it)
+        {
+            /// Exercise: Please implement the new positions of the old vertices
+            ///           - you can query for the valence of a vertex using:
+            ///                   auto valence = (*v_it).valence();
+            ///           - you can iterate over the vertices in the one-ring around a vertex v using:
+            ///                         for (auto vv_it = v_it->vertices().begin(); vv_it != v_it->vertices().end();
+            ///                         ++vv_it) {/*your code here*/}
+            ///           - the property new_pos can be used to store the new positions of the old vertices:
+            ///           new_pos_property[*v_it] = ...;
+            ///           - the formula can be found on Slideset-Surface-RepresentationsI.pdf Slide 53 or in the
+            ///           original paper:
+            ///                          https://www.graphics.rwth-aachen.de/media/papers/sqrt31.pdf
+            ///           - make sure to handle boundary (mesh->is_boundary) and feature (v_it->feature()) vertices
+            ///           accordingly
+
+            // 
+        }
+
+        // Split faces
+        std::vector<atcg::Mesh::FaceHandle> faces;
+        std::vector<atcg::Mesh::VertexHandle> centroids;
+        for(auto f_it = mesh->faces_begin(); f_it != fend; ++f_it)
+        {
+            /// Exercise: Please implement the face splits
+            ///           - you can iterate over the vertices of a face using:
+            ///                   for (auto v_it = f_it->vertices().begin(); v_it != f_it->vertices().end(); ++v_it) {
+            ///                   /*your code here*/ }
+            ///           - split the faces at the centroid of each face (mesh->split)
+            ///
+
+            // 
+        }
+
+        // Set new vertex positions
+        for(auto v_it = mesh->vertices_begin(); v_it != mesh->vertices_end(); ++v_it)
+        {
+            if(mesh->is_boundary(*v_it) || v_it->feature()) continue;
+            mesh->point(*v_it) = new_pos_property[*v_it];
+        }
+
+        // Flip old edges
+        for(auto e_it = mesh->edges_begin(); e_it != eend; ++e_it)
+        {
+            atcg::Mesh::EdgeHandle e = *e_it;
+
+            /// Exercise: Please implement the edge flips
+            ///           - make sure you check whether a flip is feasable with mesh->is_flip_ok(e)
+            ///           - make sure you don't flip an edge which is a feature edge which can be checked with
+            ///           e_it->feature
+
+            // 
+        }
+
+        // Update rendering
+        mesh->uploadData();
+    }
+
+    // This is run at the start of the program
+    virtual void onAttach() override
+    {
+        const auto& window = atcg::Application::get()->getWindow();
+        float aspect_ratio = (float)window->getWidth() / (float)window->getHeight();
+        camera_controller  = std::make_shared<atcg::CameraController>(aspect_ratio);
+
+        mesh = atcg::IO::read_mesh("res/suzanne_blender.obj");
+        mesh->uploadData();
+    }
+
+    // This gets called each frame
+    virtual void onUpdate(float delta_time) override
+    {
+        camera_controller->onUpdate(delta_time);
+
+        atcg::Renderer::clear();
+
+        if(mesh && render_faces)
+            atcg::Renderer::draw(mesh, atcg::ShaderManager::getShader("base"), camera_controller->getCamera());
+
+        if(mesh && render_points)
+            atcg::Renderer::drawPoints(mesh,
+                                       glm::vec3(0),
+                                       atcg::ShaderManager::getShader("flat"),
+                                       camera_controller->getCamera());
+
+        if(mesh && render_edges) atcg::Renderer::drawLines(mesh, glm::vec3(0), camera_controller->getCamera());
+    }
+
+    virtual void onImGuiRender() override
+    {
+        update_grid = false;
+        ImGui::BeginMainMenuBar();
+
+        if(ImGui::BeginMenu("Rendering"))
+        {
+            ImGui::MenuItem("Show Render Settings", nullptr, &show_render_settings);
+            ImGui::EndMenu();
+        }
+
+        if(ImGui::BeginMenu("Exercise"))
+        {
+            ImGui::MenuItem("Subdivision", nullptr, &show_subdivision);
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMainMenuBar();
+
+        if(show_subdivision)
+        {
+            ImGui::Begin("Settings SD", &show_subdivision);
+
+            if(ImGui::Button("Subdivide")) { subdivide_mesh(mesh); }
+
+            ImGui::End();
+        }
+
+        if(show_render_settings)
+        {
+            ImGui::Begin("Settings", &show_render_settings);
+
+            ImGui::Checkbox("Render Vertices", &render_points);
+            ImGui::Checkbox("Render Edges", &render_edges);
+            ImGui::Checkbox("Render Mesh", &render_faces);
+            ImGui::Checkbox("Render Grid", &render_grid);
+            ImGui::End();
+        }
+    }
+
+    // This function is evaluated if an event (key, mouse, resize events, etc.) are triggered
+    virtual void onEvent(atcg::Event& event) override
+    {
+        camera_controller->onEvent(event);
+
+        atcg::EventDispatcher dispatcher(event);
+        dispatcher.dispatch<atcg::FileDroppedEvent>(ATCG_BIND_EVENT_FN(Exercise03Layer::onFileDropped));
+    }
+
+    bool onFileDropped(atcg::FileDroppedEvent& event)
+    {
+        mesh = atcg::IO::read_mesh(event.getPath().c_str());
+
+        mesh->uploadData();
+
+        // Also reset camera
+        const auto& window = atcg::Application::get()->getWindow();
+        float aspect_ratio = (float)window->getWidth() / (float)window->getHeight();
+        camera_controller  = std::make_shared<atcg::CameraController>(aspect_ratio);
+
+        return true;
+    }
+
+private:
+    std::shared_ptr<atcg::Mesh> mesh;
+    std::shared_ptr<atcg::CameraController> camera_controller;
+
+    bool show_render_settings = false;
+    bool render_faces         = true;
+    bool render_points        = false;
+    bool render_edges         = false;
+    bool update_grid          = false;
+    bool render_grid          = true;
+    float voxel_size          = 0.05f;
+
+    bool show_subdivision = true;
+};
+
+class Exercise03 : public atcg::Application
+{
+public:
+    Exercise03() : atcg::Application() { pushLayer(new Exercise03Layer("Layer")); }
+
+    ~Exercise03() {}
+};
+
+atcg::Application* atcg::createApplication()
+{
+    return new Exercise03;
+}
